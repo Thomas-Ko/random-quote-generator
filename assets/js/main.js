27d1a492e1a93106628f580e9469b2e229bf5d05
Thomas-Ko <tomkolodzi@gmail.com>
2016-05-09 20:17:58 -0400

    Clean View code up and fix button-click binding issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,12 +39,16 @@ var controller = {
 	    	url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
 	    	
 	    	success: function( response ) {
-	        	console.log( response ); 
+	        	//console.log(response); 
 	        	
 	        	//parses the JSON object and returns javascript object
 	        	var obj = JSON.parse(response);
 	        	
 	        	controller.setCurrentQuote(obj);
+
+
+
+
 	    	}
 	  	}); 
 	},
@@ -56,14 +60,17 @@ var controller = {
 			"author": obj.author,
 			"category": obj.category,
 		};
+		// console.log("setCurrentQuote");
 	},
 
 	//retrieves data from model.currentQuote; will be used in view
 	getCurrentQuote: function(){
+		// console.log("getCurrentQuote");
 		return model.currentQuote;
 	}
 
 }; // end controller
+
 
 /*====================
 	VIEW
@@ -71,15 +78,42 @@ var controller = {
 
 view = {
 
- init: function(){
-    //runs after the ajax call is complete;
-	$(document).ajaxStop(function () {
-		var quoteData = controller.getCurrentQuote();
-
-    	$( "body" ).append( "<h1>" +quoteData.quote + "</h1>");
-    	$( "body" ).append( "<h2>- " +quoteData.author + "</h2>");
-    });
+ 	init: function(){   		
+   		//runs after the ajax call is complete;
+		$(document).ajaxStop(function () {
+			view.generateQuote();
+			view.buttonClick();
+    	});
   },
+
+  	generateQuote: function() {
+  		var quoteData = controller.getCurrentQuote();
+  		$("h1").html(quoteData.quote);
+  		$("h2").html("-"+quoteData.author);
+  	},
+
+  	buttonClick: function() {
+  		$("button").on("click", function(){
+  			
+  			// view.generateQuote();
+  			/*It seems I do not need to run the above function because when the view.init function is fired once (as happens on intial page load), 
+  			the .ajaxStop jQuery method ( which contains view.generate() ) will fire each
+  			subsequent time an ajax call ends without having to have view.init() fired again. */ 
+  			
+  			//Fires an ajax call
+  			controller.getQuoteInfo();
+
+  			// DO NOT REMOVE!
+  			/* Running the ajax call ( using controller.getQuoteInfo() ) everytime the button is clicked causes the controller.getQuoteInfo function
+  			to attach (bind) to the click event. The method below is needed to prevent multiple ajax calls to be attached to the click event.
+  			To better understand this, comment out the below line of code and uncomment //console.log(response)   (use CTRL+F) and, refresh the app, and
+  			click the button repeatedly and watch the console. */
+  			$( this ).off('click');
+  			/*  $( "button").unbind( "click" );   would also perform the desired action as .off. Or I could have ommitted the above .off method and the .unbind
+  			method entirely and just made the the .on method into the .one method. It's all personal preference.
+  			See more here: http://stackoverflow.com/questions/6120943/why-is-this-jquery-ajax-click-event-firing-multiple-times.*/
+  		});
+  	}
 
 }; //end view
 
